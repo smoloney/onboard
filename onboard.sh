e617b3b2bdbb7b3dc9843b0db735765fbf729de1
smoloney <s_moloney@aol.com>
2019-07-08 11:59:08 -0700

    Create onboard.sh

---
+++ onboard.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+# Jamf stores the current logged in user in $3
+HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_201.jdk/Contents/Home
+
+# General packages to install.
+PACKAGES=(
+        git
+        maven
+        nginx
+        direnv
+        ansible
+        curl   
+        tree
+        vim
+        openssl
+        python3
+        dockutil
+)
+
+# Installs linux versions of packages and ops cli tools.
+OPSPACKAGES=(
+    coreutils
+    kubernetes-cli
+    diffutils
+    ed 
+    findutils
+    gawk
+    gnu-indent
+    gnu-sed
+    gnu-tar
+    gnutils
+    grep
+    gzip
+    watch
+    wget
+)
+# Updates outated osx version of packages
+UPDATE=(
+    bash
+    make
+)
+
+function brew_install(){
+  
+   local arr="$1[@]"
+   for i in "${!arr}";
+      do
+        brew install $i
+          echo "$i"
+      done
+
+}
+
+echo "export JAVA_HOME=$HOME" >> /Users/$3/.bash_profile
+# Installing homebrew
+osascript -e '
+
+tell application "Terminal"
+    activate
+    do script ("/usr/bin/ruby -e \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\"")
+    repeat
+        delay 0.5
+        if not busy of window 1 then exit repeat
+    end repeat
+    close window 1
+end tell
+'
+brew_install PACKAGES
+brew tap homebrew/dupes
+brew_install OPSPACKAGES
+brew_install UPDATE
+
+
+cp /jarConfig/files/*policy.jar  /jarConfig/files/java.security $HOME/jre/lib/security
+cp /jarConfig/files/bcprov-jdk15on-160.jar $HOME/jre/lib/ext
+
+chown -R $3 /Users/$3/.bash_profile
+
+rm -rf jarConfig
+
+
+
+
+exit 0
+
+
+
